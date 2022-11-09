702522883d617e6bf76222695e3513bd8e7bc131
chathurperera <chathuraperera007@gmail.com>
2022-11-10 00:25:01 +0530

    user register endpoint created

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,7 +4,26 @@ const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 
 const register = asyncHandler(async (req, res) => {
-  res.send("register success");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Please provide all fields" });
+  }
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  res.send("registration successful");
 });
 
 module.exports = {

8c09a617bbfb38e33fd9cb2a94acdc8077873d51
chathurperera <chathuraperera007@gmail.com>
2022-11-09 20:51:59 +0530

    monitorModel created

---
+++ backend/controllers/upTimeTestController.js
@@ -0,0 +1,5 @@
+const asyncHandler = require('express-async-handler');
+
+const availabilityTest = asyncHandler(async(req,res) => {
+    
+})
---
+++ backend/models/monitorModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const MonitorSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    monitored: {
+      type: Boolean,
+      default: true,
+    },
+    alerts: {
+      emails: {
+        type: Array,
+      },
+      telegramAccounts: {
+        type: Array,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Monitor", MonitorSchema);

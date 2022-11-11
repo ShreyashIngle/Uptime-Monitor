f649bf3ef4bbdfc8e3ccf347080cda2437e819cf
chathurperera <chathuraperera007@gmail.com>
2022-11-12 02:25:02 +0530

    reverting back to 71b33e17cf45cee78a9c3cd4e94e58bc39c2a59f

--- backend/controllers/downtimeController.js
+++ backend/controllers/downtimeController.js
@@ -1,14 +1,12 @@
 const axios = require("axios");
 const asyncHandler = require("express-async-handler");
 const Monitor = require("../models/monitorModel");
-const fetchUrlRecursively = require("../services/fetchUrlRecursively");
+const fetchUrl = require("../services/fetchUrl");
 
 const availabilityCheck = asyncHandler(async (req, res) => {
   console.log("availabilityCheck invoked");
   const sites = await Monitor.find({}).select("url");
-  console.log("sites", sites);
-  await fetchUrlRecursively(sites);
-
+  sites.forEach((site) => fetchUrl(site));
   res.status(200).json(sites);
 });
 

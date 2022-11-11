71b33e17cf45cee78a9c3cd4e94e58bc39c2a59f
chathurperera <chathuraperera007@gmail.com>
2022-11-12 01:13:44 +0530

    fetchUrl updated

---
+++ backend/services/fetchUrl.js
@@ -0,0 +1,14 @@
+const Downtime = require("../models/downtimeModel");
+const axios = require("axios");
+
+const fetchUrl = async (site) => {
+  await axios.get(site.url).catch(async (error) => {
+    await Downtime.create({
+      monitorId: site._id,
+      statusCode: error.response.status,
+    });
+    console.log("history log created");
+  });
+};
+
+module.exports = fetchUrl;

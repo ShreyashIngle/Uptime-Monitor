1aec21adff6b3842798fb1646e896b4d2160c9d9
chathurperera <chathuraperera007@gmail.com>
2022-11-13 16:23:03 +0530

    all sites test

--- backend/controllers/downtimeController.js
+++ backend/controllers/downtimeController.js
@@ -7,14 +7,8 @@ const Downtime = require("../models/downtimeModel");
 const availabilityCheck = asyncHandler(async (req, res) => {
   console.log("availabilityCheck invoked");
   const sites = await Monitor.find({}).select("url");
-  // sites.forEach((site) => fetchUrl(site));
+  sites.forEach((site) => fetchUrl(site));
   console.log("sites", sites);
-
-  await Downtime.create({
-    monitorId: sites[0]._id,
-    statusCode: 400,
-  });
-
   res.send("<div>availabilityCheck test</div>");
 });
 

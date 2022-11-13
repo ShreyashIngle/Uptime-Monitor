2cebdc09287587ec7b7bc5849f82bf376b572b26
chathurperera <chathuraperera007@gmail.com>
2022-11-13 14:46:46 +0530

    downtime updated

--- backend/controllers/downtimeController.js
+++ backend/controllers/downtimeController.js
@@ -2,11 +2,18 @@ const axios = require("axios");
 const asyncHandler = require("express-async-handler");
 const Monitor = require("../models/monitorModel");
 const fetchUrl = require("../services/fetchUrl");
+const Downtime = require("../models/downtimeModel");
 
 const availabilityCheck = asyncHandler(async (req, res) => {
   console.log("availabilityCheck invoked");
   const sites = await Monitor.find({}).select("url");
   sites.forEach((site) => fetchUrl(site));
+
+  await Downtime.create({
+    monitorId: 1978634947967,
+    statusCode: 400,
+  });
+
   res.status(200).json(sites);
 });
 

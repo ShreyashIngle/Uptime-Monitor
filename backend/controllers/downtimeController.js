9352cc29eaba43b0bcd5c069baa23a2be2f30b89
chathurperera <chathuraperera007@gmail.com>
2022-11-14 14:55:41 +0530

    removed unnecessary code

--- backend/controllers/downtimeController.js
+++ backend/controllers/downtimeController.js
@@ -5,12 +5,12 @@ const fetchUrl = require("../services/fetchUrl");
 const Downtime = require("../models/downtimeModel");
 
 const availabilityCheck = asyncHandler(async (req, res) => {
-  console.log("availabilityCheck invoked");
   const sites = await Monitor.find({}).select("url");
+  
   for (const site of sites) {
     await fetchUrl(site);
   }
-  console.log("sites", sites);
+  
   res.send("<div>availabilityCheck test</div>");
 });
 
--- backend/routes/uptimeTestRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { availabilityCheck } = require("../controllers/upTimeTestController");
-
-router.get("/", availabilityCheck);
-
-module.exports = router;

93deef738309b1ae5a2ca9f0e1f3e4477186771c
chathurperera <chathuraperera007@gmail.com>
2022-11-17 12:47:54 +0530

    redux store setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Routes , Route } from 'react-router-dom'
 import Login from './pages/login'
 import Register from './pages/register'
-
 const App = () => {
   return (
     <Routes>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
+import store from "./app/store";
+import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );

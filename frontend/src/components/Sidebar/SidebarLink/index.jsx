1e4d1586f9b14497b1165590c3a166b70af37b1b
chathurperera <chathuraperera007@gmail.com>
2022-11-19 01:11:45 +0530

    sidebar styles added

--- frontend/src/components/Sidebar/SidebarLink/index.jsx
+++ frontend/src/components/Sidebar/SidebarLink/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import styles from "./SidebarLink.module.scss";
+import { Link } from "react-router-dom";
+import { AiOutlineSecurityScan } from "react-icons/ai";
 
 const SidebarLink = () => {
   return (
-    <div></div>
-  )
-}
+    <div className={styles.sidebarLink}>
+      <Link to="/">
+        <AiOutlineSecurityScan />
+        Monitors
+      </Link>
+    </div>
+  );
+};
 
-export default SidebarLink
+export default SidebarLink;
--- frontend/src/components/Sidebar/index.jsx
+++ frontend/src/components/Sidebar/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styles from "./Sidebar.module.scss";
-import { Link } from "react-router-dom";
-import {AiOutlineSecurityScan} from 'react-icons/ai';
-import logo from 'assets/images/logo.png'
+import SidebarLink from "components/Sidebar/SidebarLink";
+import logo from "assets/images/logo.png";
 
 const Sidebar = () => {
   return (
     <aside className={styles.aside}>
-      <img src={logo} className={styles.aside_logo} alt="" />
-      <div className={styles.sidebarLink}>
-        <Link to="/">
-                <AiOutlineSecurityScan />
-        </Link>
+      <div className={styles.aside_logo}>
+        <img src={logo} alt="" />
       </div>
+      <SidebarLink />
+      <SidebarLink />
+      <SidebarLink />
+      <SidebarLink />
+      <SidebarLink />
+      <SidebarLink />
     </aside>
   );
 };

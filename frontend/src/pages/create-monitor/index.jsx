0b8fd893a9430f092e722b04f36b0169ac5a5217
chathurperera <chathuraperera007@gmail.com>
2022-11-19 20:37:11 +0530

    create monitor styles

--- frontend/src/pages/create-monitor/index.jsx
+++ frontend/src/pages/create-monitor/index.jsx
@@ -1,10 +1,23 @@
 import BackButton from "components/BackButton";
 import React from "react";
+import styles from "./create-monitor.module.scss";
 
 const CreateMonitor = () => {
   return (
     <main>
       <BackButton />
+      <div className={styles.wrapper}>
+        <h1>Create Monitor</h1>
+        <div className={styles.wrapper_grid}>
+          <div className={styles.description}>
+            <h4>What to monitor</h4>
+            <p>Configure the target website you want to monitor.</p>
+          </div>
+          <div className={styles.inputsArea}>
+            <div className={styles.inputArea_box}></div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };

5e8518bdf3dbe3ba9f3a7b941387864e64cfea11
chathurperera <chathuraperera007@gmail.com>
2022-11-22 21:52:20 +0530

    StatsCard ui dev

--- frontend/src/pages/monitor-details/index.jsx
+++ frontend/src/pages/monitor-details/index.jsx
@@ -2,6 +2,7 @@ import BackButton from "components/BackButton";
 import React from "react";
 import Actions from "./components/Actions";
 import Head from "./components/Head";
+import StatsCard from "./components/StatsCard";
 import styles from "./monitor-details.module.scss";
 
 const MonitorDetails = () => {
@@ -10,6 +11,11 @@ const MonitorDetails = () => {
       <BackButton />
       <Head />
       <Actions />
+      <div className={styles.monitorDetails_stats}>
+        <StatsCard label="Currently up for" value="16 days 6 hours 5mins" />
+        <StatsCard label="Last checked at" value="2  minutes ago" />
+        <StatsCard label="Incidents" value="0" />
+      </div>
     </div>
   );
 };

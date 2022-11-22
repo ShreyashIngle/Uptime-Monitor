4c588891a56e66225e923e3e91e97fac4a7fd787
chathurperera <chathuraperera007@gmail.com>
2022-11-22 20:39:52 +0530

    monitor-details/components/Actions styling

--- frontend/src/pages/monitor-details/index.jsx
+++ frontend/src/pages/monitor-details/index.jsx
@@ -1,11 +1,15 @@
+import BackButton from "components/BackButton";
 import React from "react";
+import Actions from "./components/Actions";
 import Head from "./components/Head";
 import styles from "./monitor-details.module.scss";
 
 const MonitorDetails = () => {
   return (
     <div className={styles.monitorDetails}>
+      <BackButton />
       <Head />
+      <Actions />
     </div>
   );
 };

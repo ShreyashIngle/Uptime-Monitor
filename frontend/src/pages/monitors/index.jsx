0d3237623a7b1c33794d7e016873305c0b16b829
chathurperera <chathuraperera007@gmail.com>
2022-11-23 21:55:29 +0530

    back button setup

--- frontend/src/pages/monitors/index.jsx
+++ frontend/src/pages/monitors/index.jsx
@@ -1,19 +1,22 @@
 import Monitor from "components/Monitor";
 import useMonitor from "hooks/use-monitor";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./monitors.module.scss";
 
 const Monitors = () => {
   const { monitors, isLoading, isError } = useMonitor();
-
+  const navigate = useNavigate();
   return (
     <div className={styles.monitors}>
       <div className={styles.monitors_head}>
         <h2>How are you today, Chathura?</h2>
-        <button>Create monitor</button>
+        <button onClick={() => navigate("/team/create-monitor")}>
+          Create monitor
+        </button>
       </div>
       {monitors.map((monitor) => {
-        return <Monitor monitor={monitor}/>;
+        return <Monitor monitor={monitor} />;
       })}
     </div>
   );

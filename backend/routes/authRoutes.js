34c5c3cb312b71030960dfe0c12ace4fe57cba4c
chathurperera <chathuraperera007@gmail.com>
2022-12-09 20:29:38 +0530

    comments added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,8 +6,13 @@ const {
 } = require("../controllers/authController");
 const router = express.Router();
 
+//User Registration
 router.post("/register", register);
+
+//User Login
 router.post("/login", login);
+
+//Email verification
 router.get("/user/verify/:userId/:token", userVerification);
 
 module.exports = router;

15b8f24f576f5db63d647e67c22cfb6725247ab7
chathurperera <chathuraperera007@gmail.com>
2022-12-11 23:50:34 +0530

    URL validation

--- backend/controllers/monitorController.js
+++ backend/controllers/monitorController.js
@@ -1,6 +1,19 @@
 const Monitor = require("../models/monitorModel");
 const asyncHandler = require("express-async-handler");
 
+function isValidURL(str) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(str);
+}
+
 //@desc   Get Monitor
 //@route  GET /api/v1/monitor/:id
 //@access Private
@@ -36,6 +49,10 @@ const addMonitor = asyncHandler(async (req, res) => {
 
   if ((!url, !user, !team)) {
     return res.status(400).json({ message: "Provide all required fields" });
+  }
+
+  if (!isValidURL(url)) {
+    return res.status(400).json({ message: "Invalid URL" });
   }
 
   const existingMonitor = await Monitor.find({ url: url, team: team });

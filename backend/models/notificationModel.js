5b49ba20cf101a614958983a9f7ca39bc1defc46
chathurperera <chathuraperera007@gmail.com>
2022-12-29 14:46:00 +0530

    notificationModel created

---
+++ backend/models/notificationModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const NotificationSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      default: false,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Notification", NotificationSchema);

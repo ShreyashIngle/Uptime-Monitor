5769412a28e98b603c77eaa8a77975da6ce3d7a0
chathurperera <chathuraperera007@gmail.com>
2023-01-26 04:30:48 +0530

    styles update

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -26,11 +26,14 @@ const Login = () => {
  const { isLoading, isError, isSuccess, message } = useSelector(
    (state) => state.auth
  );
+
+
  
   useEffect(() => {
     if (isError) {
       toast.error(message);
     }
+
     const user = localStorage.getItem("user");
 
     if (user) {

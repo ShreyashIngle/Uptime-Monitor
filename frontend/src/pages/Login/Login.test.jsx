94b09fe810cc51dccdd1d03e0d6c2bc2546c106f
chathurperera <chathuraperera007@gmail.com>
2023-02-08 17:53:29 +0530

    test: new tests added

--- frontend/src/pages/Login/Login.test.jsx
+++ frontend/src/pages/Login/Login.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { beforeAll, beforeEach, describe, expect, it } from "vitest";
 import Login from ".";
 import { BrowserRouter } from "react-router-dom";
@@ -13,14 +13,32 @@ beforeEach(() => {
       </Provider>
     </BrowserRouter>
   );
-})
+});
 
 it("title should be visible", () => {
-  const title = screen.queryByText(/Enter your credentials to access your account/);
+  const title = screen.queryByText(
+    /Enter your credentials to access your account/
+  );
   expect(title).toBeVisible();
 });
 
-it("email input filed should be empty on render",() => {
+it("email input filed should be empty on render", () => {
   const emailInput = screen.getByPlaceholderText("name@company.com");
   expect(emailInput.value).toBe("john@gmail.com");
-})
+});
+
+it("email input should change", () => {
+  const emailInput = screen.getByPlaceholderText("name@company.com");
+  const testValue = "someone@gmail.com";
+
+  fireEvent.change(emailInput, { target: { value: testValue } });
+  expect(emailInput.value).toBe(testValue);
+});
+
+it("password input should change", () => {
+  const emailInput = screen.getByPlaceholderText("name@company.com");
+  const testValue = "someone@gmail.com";
+
+  fireEvent.change(emailInput, { target: { value: testValue } });
+  expect(emailInput.value).toBe(testValue);
+});

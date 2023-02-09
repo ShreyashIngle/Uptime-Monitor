c4040e7533fc9f7dc24c25686c6780477bf9733c
chathurperera <chathuraperera007@gmail.com>
2023-02-10 00:00:27 +0530

    build path updates

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/dist"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend/dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
   });
 }
 

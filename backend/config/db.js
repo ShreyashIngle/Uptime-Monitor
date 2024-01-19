dcf70dd1128aa5da5cd95be65dc19011430b29e9
shreyashingle2004@gmail.com <shreyashingle2004@gmail.com>
2024-01-19 18:12:12 +0530

    Adding uptime monitor

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = (URI) => {
-  mongoose.connect(URI, {
+  mongoose.connect('mongodb+srv://pratham:pratham902@pratham.s4wuhkp.mongodb.net/shreyash', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
